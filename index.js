8a6a83be76370fa31858626d935ce341c5c774f4
Mordechai <mordwein77@gmail.com>
2022-09-22 00:04:14 -0400

    commit tree 1bc55f97dfc0425272daea52c7bc64e6ded1f134

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData (name, email){
+
+return fetch("http://localhost:3000/users", {
+method: "POST",
+headers:{
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+},
+body: JSON.stringify({
+    name,
+    email,
+    
+}),
+ })
+ .then (function (response){
+   return response.json();
+    
+ })
+ .then(function (object){
+console.log(object.id);
+document.body.innerHTML = object.id
+ })
+ .catch (function (message){
+ document.body.innerHTML = message
+ });
+
+
+}
